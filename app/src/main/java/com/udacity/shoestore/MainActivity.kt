a60e1c5a25158b12c1ab1d394303c436b15b0aa8
Joseph <osekuemen@gmail.com>
2021-06-22 21:35:54 +0100

    Step 16 - Navigating Up

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -3,6 +3,9 @@ package com.udacity.shoestore
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
@@ -16,6 +19,20 @@ class MainActivity : AppCompatActivity() {
         // Using DataBindingUtil to inflate layout
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
+        // Find the navController from myNavHostFragment
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        // Link the navController to our ActionBar by calling NavigationUI.setupActionBarWithNavController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
         Timber.plant(Timber.DebugTree())
+    }
+
+    // Override onSupportNavigateUp
+    // Find the navController and then call navController.navigateUp
+    // This is where we need the navigation to handle what happens when up is pressed, otherwise
+    // nothing will happen when we press the navigation button
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
